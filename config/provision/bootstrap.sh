f42f1683f466e788791c8f4d25421c31b70e387d
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-07-13 21:35:38 +0300

    Installs Apache http server and PHP 7
        As well installs some of the PHP 7 packages.

--- config/provision/bootstrap.sh
+++ config/provision/bootstrap.sh
@@ -24,3 +24,12 @@ sudo apt -y upgrade
 # ***************** Installs Handy Utilities ***************** #
 sudo apt -y install vim htop pcregrep unzip siege curl
 # ************************************************************ #
+
+# *************** Installs Apache2 HTTP Server *************** #
+sudo apt -y install apache2
+# ************************************************************ #
+
+# ********************** Installs PHP 7 ********************** #
+sudo apt -y install php7.0 php7.0-cli php7.0-curl php7.0-json php7.0-xml \
+    php7.0-mcrypt php7.0-mbstring php7.0-mysql php7.0-pgsql php7.0-intl
+# ************************************************************ #

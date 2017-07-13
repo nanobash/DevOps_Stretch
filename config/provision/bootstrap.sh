111e0777c72b78145d0949697447c38c2fccdba7
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-07-13 19:10:46 +0300

    Adds basic update operations to bootstap.sh

--- config/provision/bootstrap.sh
+++ config/provision/bootstrap.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
-# ************************************************************
-# *        Maintains debian/stretch Operating System         *
-# ************************************************************
+# ************************************************************ #
+# *        Maintains debian/stretch Operating System         * #
+# ************************************************************ #
+
+# ************************** Basics ************************** #
+# Fixes broken dependencies
+sudo apt -f install
+
+# Removes unused packages
+sudo apt -y autoremove
+
+# Cleans repository cache
+sudo apt clean all
+
+# Update packages
+sudo apt -y update
+
+# Upgrade packages
+sudo apt -y upgrade
+# ************************************************************ #

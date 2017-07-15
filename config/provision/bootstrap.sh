74e3ae1da8e0fb07d5e97184586e8984a1b3819f
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-07-15 15:48:45 +0300

    Installs symfony installer

--- config/provision/bootstrap.sh
+++ config/provision/bootstrap.sh
@@ -131,6 +131,13 @@ if [ ! -f /usr/bin/composer ]; then
     php -r "unlink('composer-setup.php');"
     sudo mv composer.phar /usr/bin/composer
 fi
+
+# Installs Symfony PHP framework installer
+if [ ! -f /usr/local/bin/symfony ]; then
+    sudo mkdir -p /usr/local/bin
+    sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+    sudo chmod a+x /usr/local/bin/symfony
+fi
 # ************************************************************ #
 
 # ********************* Restart Services ********************* #
